034584b81170200f632b4c3610cf69271d1f63cf
Roi Ardao <roi.ardao@gmail.com>
2025-09-04 17:31:44 +0200

    refactor dino FSM, add stump obstacle, hit animation

--- scenes/dinos/dino.gd
+++ scenes/dinos/dino.gd
@@ -24,22 +24,34 @@ var is_enabled: bool = false
 func _ready() -> void:
 	collision_duck.disabled = true
 	is_enabled = false
-	current_state = State.IDLE
-	var timer_node = get_node("../StartTimer")
-	timer_node.timeout.connect(enable_dino.bind())
+	change_state(State.IDLE)
+	var timer_node = get_parent().find_child("StartTimer")
+	if timer_node != null:
+		timer_node.timeout.connect(enable_dino.bind())
 
 
 func _physics_process(delta: float) -> void:
 	if is_enabled:
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			current_state = State.JUMP
-		elif Input.is_action_pressed("ui_down") and is_on_floor():
-			current_state = State.DUCK
-		elif not is_on_floor():
-			velocity += get_gravity() * 1.5 * delta
-		else:
-			current_state = State.RUN
+		match current_state:
+			State.JUMP:
+				if not is_on_floor():
+					velocity += get_gravity() * 1.5 * delta
+				else:
+					change_state(State.RUN)
+			State.RUN:
+				if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+					velocity.y = JUMP_VELOCITY
+					change_state(State.JUMP)
+				elif Input.is_action_pressed("ui_down") and is_on_floor():
+					change_state(State.DUCK)
+			State.DUCK:
+				if Input.is_action_just_released("ui_down"):
+					change_state(State.RUN)
+			State.HIT:
+				if not is_on_floor():
+					velocity += get_gravity() * 1.5 * delta
+
+
 	check_collision_shape()
 	play_animation()
 	move_and_slide()
@@ -60,3 +72,18 @@ func play_animation() -> void:
 
 func enable_dino() -> void:
 	is_enabled = true
+
+
+func change_state(new_state: State) -> void:
+	current_state = new_state
+
+
+func on_obstacle_hit(body: Node2D) -> void:
+	change_state(State.HIT)
+
+
+func on_hit_animation_end() -> void:
+	if not is_on_floor():
+		change_state(State.JUMP)
+	else:
+		change_state(State.RUN)
--- scenes/game.gd
+++ scenes/game.gd
@@ -3,10 +3,14 @@ extends Node
 
 const DINO_START_POS := Vector2i(100, 554)
 const CAMERA_START_POS := Vector2i(576, 324)
+const GO_SIGNAL := preload("uid://cmb41isywt1hk")
 const GO_SIGNAL_START_POS := Vector2i(300, 564)
 const SPEED := 10.0
-const GO_SIGNAL := preload("uid://cmb41isywt1hk")
 
+@export_category("Obstacles")
+@export var barrel_scene: PackedScene
+@export var box_scene: PackedScene
+@export var stump_scene: PackedScene
 
 var is_started: bool = false
 var screen_size: Vector2i
@@ -16,10 +20,10 @@ var screen_size: Vector2i
 @onready var spawns: Node2D = %Spawns
 @onready var ground: StaticBody2D = %Ground
 @onready var start_timer: Timer = %StartTimer
+@onready var spawn_timer: Timer = %SpawnTimer
 
 
 func _ready() -> void:
-	start_timer.timeout.connect(start_game.bind())
 	is_started = false
 	screen_size = get_window().size
 	create_go_signal()
@@ -35,14 +39,6 @@ func _physics_process(_delta: float) -> void:
 		ground.position.x += screen_size.x
 
 
-func start_game() -> void:
-	is_started = true
-
-
-func _on_despawner_body_entered(body: Node2D) -> void:
-	body.queue_free()
-
-
 func create_go_signal() -> void:
 	var go_signal := GO_SIGNAL.instantiate()
 	go_signal.global_position = GO_SIGNAL_START_POS
@@ -51,5 +47,29 @@ func create_go_signal() -> void:
 
 func restart_game() -> void:
 	dino.position = DINO_START_POS
-
 	create_go_signal()
+
+
+func spawn_obstacle(scene: PackedScene) -> void:
+	var obstacle = scene.instantiate()
+	obstacle.position = spawns.get_child(2).position
+	add_child(obstacle)
+	obstacle.body_entered.connect(dino.on_obstacle_hit.bind())
+
+
+func start_game() -> void:
+	is_started = true
+	spawn_timer.start()
+	dino.current_state = dino.State.RUN
+
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_obstacle(stump_scene)
+
+
+func _on_despawner_area_entered(area: Area2D) -> void:
+	area.queue_free()
+	
+
+func _on_despawner_body_entered(body: Node2D) -> void:
+	body.queue_free()
